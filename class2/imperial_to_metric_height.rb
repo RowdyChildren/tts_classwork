1595f1c82f623521c941579e0de9555f9c323abe
Riley Childs <riley@tfsgeo.com>
2014-11-05 19:56:50 -0500

    made imp interactive

--- class2/imperial_to_metric_height.rb
+++ class2/imperial_to_metric_height.rb
@@ -1,13 +1,14 @@
 puts "What is your name?"
-my_name = gets
+my_name = gets.chomp
 #my_name = 'Riley Childs'
 puts "What is your height in inches"
-height_inches = gets
+height_inches = gets.to_i
 
-weight_pounds = 120
+puts "How much do you weigh in pounds?"
+weight_pounds = gets.to_i
 
 height_centimeters = height_inches * 2.54
 
 weight_kilograms = weight_pounds * 0.453592
 
-puts my_name + ' is ' + height_centimeters.to_s + ' cm and ' + weight_kilograms.to_s + ' kg. '
+puts my_name + ' is ' + height_centimeters.to_s + ' tall in cm and weighs ' + weight_kilograms.to_s + ' in kg. '
