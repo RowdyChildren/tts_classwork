07b7a4085f257152a7f327d21b0bb9850cef3f1a
Riley Childs <riley@tfsgeo.com>
2014-11-05 19:58:16 -0500

    add comments to imp

--- class2/imperial_to_metric_height.rb
+++ class2/imperial_to_metric_height.rb
@@ -1,14 +1,17 @@
+#Prompt for data
 puts "What is your name?"
+#get the data
 my_name = gets.chomp
 #my_name = 'Riley Childs'
+#get more data
 puts "What is your height in inches"
 height_inches = gets.to_i
-
+#more data
 puts "How much do you weigh in pounds?"
 weight_pounds = gets.to_i
-
+#convert data
 height_centimeters = height_inches * 2.54
 
 weight_kilograms = weight_pounds * 0.453592
-
+#display data
 puts my_name + ' is ' + height_centimeters.to_s + ' tall in cm and weighs ' + weight_kilograms.to_s + ' in kg. '
