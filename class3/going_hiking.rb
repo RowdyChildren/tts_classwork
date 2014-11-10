9e4b889a5c6bcdb87b4875eb688eae1d7d324d13
Riley Childs <riley@tfsgeo.com>
2014-11-10 18:53:06 -0500

    added some logic and changed the program a little

--- class3/going_hiking.rb
+++ class3/going_hiking.rb
@@ -1,15 +1,44 @@
-def going_hiking(temp)
-	if temp >= 50
-		words = "Lets go hiking"
-	elsif temp < 50
-		words = "That's too cold"
-	end
-	return words
-end
+# def going_hiking(temp)
+# 	if temp >= 50
+# 		words = "Lets go hiking"
+# 	elsif temp < 50
+# 		words = "That's too cold"
+# 	end
+# 	return words
+# end
 			
-puts "What is the temp today?"
-current_temp = gets.chomp.to_i
-#error checking goes here
-puts going_hiking(current_temp)
+# puts "What is the temp today?"
+# current_temp = gets.chomp.to_i
+# #error checking goes here
+# puts going_hiking(current_temp)
+
+def going_hiking (cur_temp)
+	if cur_temp == 23
+		puts "It is 23 Degrees!"
+		puts "Is 23 Degrees a good tempature for hiking? (y/n)"
+		user_o = gets.chomp
+		if user_o == 'n'
+			puts "Yes that is very cold!"
+			puts "What is a a good tempature for hiking?"
+			user_t = gets.chomp.to_i
+			if user_t >= 50 
+				puts "#{user_t} is a good tempature for hiking"
+				return
+			elsif user_t < 50
+				puts "#{user_t} is much too cold"
+				
+			end
+		elsif user_o == 'y'
+			puts "I disagree 23 degrees is too cold"
+		end
+	end
+end
 
 
+
+
+
+
+cur_temp = 23
+
+going_hiking (cur_temp)
