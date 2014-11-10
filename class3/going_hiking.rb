3e0d75c5ae61fbfcc04fc29d9d6c7527d2c34708
Riley Childs <riley@tfsgeo.com>
2014-11-10 18:30:42 -0500

    removed excesive logic

--- class3/going_hiking.rb
+++ class3/going_hiking.rb
@@ -1,14 +1,14 @@
 def going_hiking(temp)
-	if temp >= 50.0
+	if temp >= 50
 		words = "Lets go hiking"
-	elsif temp < 50.0
+	elsif temp < 50
 		words = "That's too cold"
 	end
 	return words
 end
 			
 puts "What is the temp today?"
-current_temp = gets.chomp.to_f
+current_temp = gets.chomp.to_i
 #error checking goes here
 puts going_hiking(current_temp)
 
