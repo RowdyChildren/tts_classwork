2ac1416393a35b740ed1aaf2b1722e8fdb001fa0
Riley Childs <riley@tfsgeo.com>
2014-11-10 20:47:24 -0500

    added birds on a wire program

---
+++ class3/count.rb
@@ -0,0 +1,7 @@
+puts "Riley's count program"
+count = 1
+puts "#{count} bird on a wire!"
+for count in 2 .. 10
+
+	puts "#{count} birds on a wire!"
+end
---
+++ class3/lone.rb
@@ -0,0 +1,8 @@
+x = 0
+while x<=10
+puts "#{x} is the lonliest number"
+if x == 10
+	puts "We are exiting the loop"
+end
+x  += 1
+end
