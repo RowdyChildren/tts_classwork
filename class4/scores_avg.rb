60d61d8b41e3e8983a805add3e30191c682ed8b2
Riley Childs <riley@tfsgeo.com>
2014-11-12 19:02:44 -0500

    new program

--- class4/scores_avg.rb
+++ class4/scores_avg.rb
@@ -1,12 +1,20 @@
-scores = [5,6,8,10]
-
+scores = []
+NumOfScores = 4
 counter = 0
+while NumOfScores > counter
+	system "cls" or "clear"
+	puts "Input 4 Sports Scores"
+	puts "Give me a Sports Score"
+	scores.push(gets.chomp.to_i)
+	counter += 1
+end
+
 sum = 0
 
 scores.each do |score|
    sum = sum.to_f + score.to_f
-   counter += 1.0
+   
 end
 
-avg = sum/counter
+avg = sum/scores.count.to_f
 puts "Avg is #{avg}"
