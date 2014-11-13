812f7ec03410e4e5a74af38ed2dbe7fdcee338ce
Riley Childs <riley@tfsgeo.com>
2014-11-12 20:31:29 -0500

    Created RPS Program added test function

--- class4/rps.rb
+++ class4/rps.rb
@@ -1,19 +1,14 @@
 def check_win(one, two)
 	if one.downcase == "scissors" and two.downcase == "paper"
 		return 1
-	
 	elsif one.downcase == "paper" and two.downcase == "rock"
 		return 1
-	
 	elsif one.downcase == "rock" and two.downcase == "scissors"
 		return 1
-	
 	elsif two.downcase == "scissors" and one.downcase == "paper"
 		return 2
-	
 	elsif two.downcase == "paper" and one.downcase == "rock"
 		return 2
-	
 	elsif two.downcase == "rock" and one.downcase == "scissors"
 		return 2
 	elsif two.downcase == one.downcase
@@ -35,9 +30,9 @@ def generate_prompts()
 	return gets.chomp
 end
 def integer?(str)
+
   /\A[+-]?\d+\z/ === str
 end
-
 puts "Lets play rock paper scissors, how many times do you want to play?"
 times = gets.chomp
 while integer?(times) == false
@@ -51,12 +46,14 @@ win_times = win_times.to_i
 count = 0
 w1 = 0
 w2 = 0
-
 while count <= times
 count += 1
 
-#rps = ["rock", "paper", "scissors"]
+rps = ["rock", "paper", "scissors"]
+if ARGV[0].downcase == "test"
 rps = ["scissors"]
+puts "TEST MODE: computer will always play scissors!"
+end
 computer = rps.shuffle[0]
 puts "Let's Play Rock Paper Scissors!"
 one = generate_prompts()
